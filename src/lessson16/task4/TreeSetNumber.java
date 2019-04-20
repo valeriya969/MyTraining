1dcef804133a6ed2f882a917e3742e774ffe6a0b
Valeriya <valeriyavino@gmail.com>
2019-04-20 13:01:12 +0900

    New example of set

---
+++ src/lessson16/task4/TreeSetNumber.java
@@ -0,0 +1,24 @@
+package lessson16.task4;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TreeSetNumber {
+    public static void main(String[] args) {
+        System.out.println("Введите целые числа");
+        sortedNumbers();
+    }
+
+    private static void sortedNumbers() {
+        Set<String> integers = new HashSet<>();
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()){
+            integers.add(matcher.group());
+        }
+        System.out.println(integers);
+    }
+}
